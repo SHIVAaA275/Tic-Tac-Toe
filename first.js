3a4e7ca4ccd97a6ce899b683d49635e6ca26a7c2
SHIVAaA275 <siddharththati@gmail.com>
2026-06-11 14:43:40 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,119 +1,33 @@
-let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
-
-let turnO = true;
-
-let oMoves = [];
-let xMoves = [];
-
-const winPatterns = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [0, 4, 8],
-  [1, 4, 7],
-  [2, 5, 8],
-  [2, 4, 6],
-  [3, 4, 5],
-  [6, 7, 8],
-];
-
-const resetGame = () => {
-  turnO = true;
-
-  oMoves = [];
-  xMoves = [];
-
-  enableBoxes();
-
-  msgContainer.classList.add("hide");
-};
-
-boxes.forEach((box) => {
-  box.addEventListener("click", () => {
-
-    if (turnO) {
-      box.innerText = "O";
-      box.style.color = "green";
-
-      oMoves.push(box);
-
-      if (oMoves.length > 3) {
-        let oldestBox = oMoves.shift();
-
-        oldestBox.innerText = "";
-        oldestBox.disabled = false;
-      }
-
-      turnO = false;
-    } else {
-      box.innerText = "X";
-      box.style.color = "orange";
-
-      xMoves.push(box);
-
-      if (xMoves.length > 3) {
-        let oldestBox = xMoves.shift();
-
-        oldestBox.innerText = "";
-        oldestBox.disabled = false;
-      }
-
-      turnO = true;
-    }
-
-    box.disabled = true;
-
-    checkWinner();
-  });
-});
-
-const disableBoxes = () => {
-  boxes.forEach((box) => {
-    box.disabled = true;
-  });
-};
-
-const enableBoxes = () => {
-  boxes.forEach((box) => {
-    box.disabled = false;
-    box.innerText = "";
-  });
-};
-
-const showWinner = (winner) => {
-  msg.innerText = `🎉 Congratulations! Winner is ${winner}`;
-  msgContainer.classList.remove("hide");
-
-  disableBoxes();
-};
-
-const checkWinner = () => {
-  for (let pattern of winPatterns) {
-
-    let pos1Val = boxes[pattern[0]].innerText;
-    let pos2Val = boxes[pattern[1]].innerText;
-    let pos3Val = boxes[pattern[2]].innerText;
-
-    if (
-      pos1Val !== "" &&
-      pos2Val !== "" &&
-      pos3Val !== ""
-    ) {
-      if (
-        pos1Val === pos2Val &&
-        pos2Val === pos3Val
-      ) {
-        showWinner(pos1Val);
-        return true;
-      }
-    }
-  }
-
-  return false;
-};
-
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click", resetGame);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOM</title>
+    <link rel="stylesheet" href="web.css">
+</head>
+<body>
+    <div class="msg-container hide">
+        <p id="msg"></p>
+        <button id="new-btn">New Game</button>
+    </div>
+    <main>
+    <h1>Tic Tac Toe</h1>
+    <div class="container">
+        <div class="game">
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+            <button class="box"></button>
+        </div>
+    </div>
+    <button id="reset-btn">Reset</button>
+    <script src="first.js"></script>
+    </main>
+</body>
+</html>
